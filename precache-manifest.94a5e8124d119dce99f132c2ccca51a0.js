0f88d905e00edb6e40c01efd5d95c2c4bceb84ff
Gloria Zhong <67024697+gloriagb@users.noreply.github.com>
2021-02-26 16:31:54 -0800

    Updates

---
+++ precache-manifest.94a5e8124d119dce99f132c2ccca51a0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e0ab8571201e7dbb7f21560aaeaa57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ffdf4c4a135e9dfe95",
+    "url": "/static/css/main.25ac248d.chunk.css"
+  },
+  {
+    "revision": "b0d766d9bc1426570107",
+    "url": "/static/js/2.69f27dbc.chunk.js"
+  },
+  {
+    "revision": "b6a27d744c2b1118b658bf5e33ce6251",
+    "url": "/static/js/2.69f27dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ffdf4c4a135e9dfe95",
+    "url": "/static/js/main.7a2180c9.chunk.js"
+  },
+  {
+    "revision": "f736c91889481d979fb1",
+    "url": "/static/js/runtime-main.c1f481db.js"
+  },
+  {
+    "revision": "d4d1ae500d96a71cad765cc4f7b44e61",
+    "url": "/static/media/about-self.d4d1ae50.jpg"
+  },
+  {
+    "revision": "e3403ebea80bfecd3d48b7ef87b1156f",
+    "url": "/static/media/affinity-diagram.e3403ebe.png"
+  },
+  {
+    "revision": "8b9ef979e9a7c3b3901768b4175bc465",
+    "url": "/static/media/arrow.8b9ef979.svg"
+  },
+  {
+    "revision": "73a59fe90f2ac1fbf77c8e7d3be56dce",
+    "url": "/static/media/beedie-home-cover.73a59fe9.png"
+  },
+  {
+    "revision": "8e91e5a40be21985b7d1be89742671a3",
+    "url": "/static/media/codesigners.8e91e5a4.png"
+  },
+  {
+    "revision": "363ed5b38e5c2d9678dcc98471b75cb1",
+    "url": "/static/media/fh-home-small.363ed5b3.png"
+  },
+  {
+    "revision": "a2605549f967078ff35a41d682d26117",
+    "url": "/static/media/fh-home.a2605549.png"
+  },
+  {
+    "revision": "f814fe813da4ea015e8422199b61f84c",
+    "url": "/static/media/fh-icons-drafts.f814fe81.png"
+  },
+  {
+    "revision": "68eb7e4647632b423cfcbdc4bb3cff48",
+    "url": "/static/media/fh-layout.68eb7e46.png"
+  },
+  {
+    "revision": "f11c7d44b88b4301bd1a71b26058085d",
+    "url": "/static/media/flexride-cover.f11c7d44.png"
+  },
+  {
+    "revision": "b6fd7fd1f23f8c8a55031f28d8a28818",
+    "url": "/static/media/logo.b6fd7fd1.svg"
+  },
+  {
+    "revision": "be5e4b3ced3f9ab33ab7e71f90665852",
+    "url": "/static/media/media-kits.be5e4b3c.png"
+  },
+  {
+    "revision": "f9d9f97b8cde767dbba8547df43bdc1d",
+    "url": "/static/media/musers-sample.f9d9f97b.png"
+  },
+  {
+    "revision": "32e27e906434bc59b5faa5da0c74d68c",
+    "url": "/static/media/onboarding.32e27e90.png"
+  },
+  {
+    "revision": "e1a95284efcdd87d9426d56e9518be10",
+    "url": "/static/media/portfolio-details.e1a95284.png"
+  },
+  {
+    "revision": "b51cddf6c049a548259d48920c1536d3",
+    "url": "/static/media/portfolio.b51cddf6.png"
+  },
+  {
+    "revision": "843889eda78f40e3bc8f6d0d2287a8c0",
+    "url": "/static/media/presentation-slide.843889ed.png"
+  },
+  {
+    "revision": "cf993467c42b179264478bdb9e941a83",
+    "url": "/static/media/profile-analysis.cf993467.png"
+  },
+  {
+    "revision": "c6967744ec875413bcf8c69faabd9d75",
+    "url": "/static/media/profile-iterations.c6967744.png"
+  },
+  {
+    "revision": "2419734ce03c9310e84b6683c4bbe986",
+    "url": "/static/media/profile.2419734c.png"
+  },
+  {
+    "revision": "352a44fd7c593252aa6a73e1c99eb1ce",
+    "url": "/static/media/revisions.352a44fd.png"
+  },
+  {
+    "revision": "d52fd6370477f15508b0e4725f95ca9c",
+    "url": "/static/media/self-portrait-circle-mono.d52fd637.png"
+  },
+  {
+    "revision": "38f7b29fd67fec8b7c07b86db470d24f",
+    "url": "/static/media/sketches.38f7b29f.png"
+  },
+  {
+    "revision": "ddde4d2415aa5843d6711ff54808cdb9",
+    "url": "/static/media/support-admin.ddde4d24.png"
+  },
+  {
+    "revision": "f5d574ba302d6518bcbe53043f31d6c9",
+    "url": "/static/media/support-home-edit.f5d574ba.png"
+  },
+  {
+    "revision": "c4393328d9af0925580286a40bf9d729",
+    "url": "/static/media/support-home.c4393328.png"
+  },
+  {
+    "revision": "01a026e3f7094f0068514f46d9d09747",
+    "url": "/static/media/support-original2-edit.01a026e3.png"
+  },
+  {
+    "revision": "da34058c299425b3d05e9ebe9402d0d2",
+    "url": "/static/media/website.da34058c.png"
+  },
+  {
+    "revision": "007980c8e056fc3166f301f4d86265a8",
+    "url": "/static/media/workshop.007980c8.png"
+  }
+]);
